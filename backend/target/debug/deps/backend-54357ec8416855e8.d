823487a77d833edddb3d6a44da594394e1976123
Temidaradev <ahmet.affan.ebcioglu@outlook.com>
2026-03-28 11:15:15 +0300

    Delete backend/target directory

--- backend/target/debug/deps/backend-54357ec8416855e8.d
+++
@@ -1,5 +0,0 @@
-/Users/lidldev/Documents/tua-astro-hackathon/backend/target/debug/deps/backend-54357ec8416855e8.d: src/main.rs
-
-/Users/lidldev/Documents/tua-astro-hackathon/backend/target/debug/deps/libbackend-54357ec8416855e8.rmeta: src/main.rs
-
-src/main.rs:
