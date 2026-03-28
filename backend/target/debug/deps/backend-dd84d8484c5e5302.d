319f56e2bab7d19c706dbc8b8bcb311ce479dde7
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2026-03-28 11:13:27 +0300

    backend init

---
+++ backend/target/debug/deps/backend-dd84d8484c5e5302.d
@@ -0,0 +1,5 @@
+/Users/lidldev/Documents/tua-astro-hackathon/backend/target/debug/deps/backend-dd84d8484c5e5302.d: src/main.rs
+
+/Users/lidldev/Documents/tua-astro-hackathon/backend/target/debug/deps/libbackend-dd84d8484c5e5302.rmeta: src/main.rs
+
+src/main.rs:
